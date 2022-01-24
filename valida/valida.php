d623ead41487f613ba2d7726142a8291f4d5ce68
tcigor <31429293+tcigor@users.noreply.github.com>
2022-01-24 15:19:30 -0300

    Add files via upload

--- valida/valida.php
+++ valida/valida.php
@@ -1,1 +1,17 @@
+<?php
+/* --------------------------------------------
+ * Programa para validar login no site
+ * --------------------------------------------
+*/
+        
+    //Recebe os dados do index.html
+    echo "<h2>Você digitou:</h2><h3>".$_POST["login"]." - ".$_POST["password"]."</h3>";
 
+    //Faz conexão com o banco de dados
+    include("../../config/conecta_db.php");
+    echo "<p>Login no banco efetuado com sucesso.</p>";
+
+    //Dependendo do resultado, entra ou sai do sistema
+
+    echo "<p><a href='../'>Volta</a></p>";
+?>
